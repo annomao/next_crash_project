438b9071591fad03236a4cf27a294d285ee85ade
annomao <omaoannkerubo@gmail.com>
2023-05-11 02:20:24 +0300

    use fetch api to fetch display data

--- app/code/repos/page.js
+++ app/code/repos/page.js
@@ -1,10 +1,34 @@
 export const metadata = {
     title: 'Repos'
 }
-const ReposPage = () => {
+
+//fetch repo data using the fetch api
+
+const fetchRepos = async () =>{
+    const res = await fetch('https://api.github.com/users/annomao/repos')
+    if (!res.ok){
+        throw new Error('failed to fetch data')
+    }
+
+    return res.json()
+}
+
+const ReposPage =  async () => {
+    const repos = await fetchRepos()
+
   return (
-    <div>
-      Display repos
+    <div className="repos-container"> 
+    <h1>REPOSITORIES LIST</h1>
+        <ol className="repo-list">
+      {repos.map((repo)=>{
+
+        return <li key={repo.id}>
+            <h1>{repo.name}</h1>
+            <p>{repo.html_url}</p>
+            <p>{repo.description}</p>
+        </li>
+      })}
+      </ol>
     </div>
   )
 }
